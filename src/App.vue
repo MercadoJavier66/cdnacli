4081625c292d058e6084ed6bc04e73640d55926f
MercadoJavier66 <javiermercado66@hotmail.com>
2022-05-16 00:41:49 -0300

    cdnacli

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
+
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Esto es Cli con Vue 2"/>
+    <h3>monty pierde la cabeza</h3>
+
+    <HelloWorld msg="hola" >
+
+    </HelloWorld >
+    <Vehiculo >
+    </Vehiculo >
+
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Vehiculo from './components/Vehiculo.vue';
 
 export default {
-  name: 'App',
+name: 'App',
   components: {
-    HelloWorld
-  }
-}
+      HelloWorld,
+    Vehiculo,
+  },
+
+// data() {
+//     return{
+//          numeroA: 1
+//     }
+//   }
+};
+
+
 </script>
 
 <style>
---
+++ src/components/Vehiculo.vue
@@ -0,0 +1,109 @@
+<template>
+<div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Vehiculo',
+    props:{
+        titulos: {
+            type: Array,
+            required: true
+        },
+        vehiculo:{
+            type: Array,
+            required: true
+        },
+        estilos:{
+            type: String,
+            required: true
+        }
+
+    },
+};
+data:{
+      tabla1: {
+            titulos: ["Opcion", "Marca", "Modelo", "Precio"],
+            vehiculo [
+                {
+                    marca: 'Fiat',
+                    modelo: ' Uno Way',
+                    precio: 850000
+                },
+                {
+                    marca: 'Ford',
+                    modelo: 'Fiesta Kinetic',
+                    precio: 1750000
+                },
+                {
+                    marca: 'Volskwagen',
+                    modelo: 'Nivus',
+                    precio: 2600000
+                }
+
+            ]
+
+      };
+        tabla2: {
+            titulos: ['Opcion', 'Motor', 'Caja', 'Combustible'],
+            vehiculo [
+                {
+                    marca: 'Motor 1.4',
+                    modelo: '5 velocidades Manual',
+                    precio: 45 //combustible
+                },
+                {
+                    marca: 'Motor 1.6',
+                    modelo: '6 velocidades Automatica',
+                    precio: 52
+                },
+                {
+                    marca: 'Motor 1.0',
+                    modelo: '6 Velocidades Manual',
+                    precio: 55
+                }
+            ]
+        };
+        tabla3: {
+            titulos: ['Opcion', 'GPS', 'ABS', 'ESP'],
+            vehiculo [
+                {
+                    marca: 'No',
+                    modelo: 'Si',
+                    precio: 'No'
+                },
+                {
+                    marca: 'Si',
+                    modelo: 'Si',
+                    precio: 'No'
+                },
+                {
+                    marca: 'Si',
+                    modelo: 'Si',
+                    precio: 'Si'
+                }
+            ]
+        };
+
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
